2049cccaad5c50c2a68fb8f5642111ede74939c7
Abele Atresso <abele.atresso123@gamil.com>
2020-06-29 18:38:40 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+// JavaScript goes here. 
+// This is for class 6 lab. 
+
+
+var userName = prompt('please enter your first name.'); 
+
+
+alert('Hello, ' + userName + ' click OK to proceed.');
+
+var hourNow = prompt('What time is it, friend? Use Military time.'); 
+
+var greeting; 
+
+if(hourNow >= 0 && hourNow < 7){ 
+ greeting = ' Good Morning, ' + userName;
+} else if (hourNow >= 7 && hourNow <= 12){
+ greeting = ' Good afternoon ' + userName; 
+} else if(hourNow >= 12 && hourNow <= 18){
+  greeting = ' Good mid day ' + userName; 
+} else if(hourNow >= 18 && hourNow <= 24){
+  greeting = ' Good Evening ' + userName; 
+} else {
+  'Something went wrong!';
+}
+
+document.write('<h1 style="color:blue">' + greeting + '</h1');
+
+/* 
+This script is displays a greeting to the user based on the users input. */ 
