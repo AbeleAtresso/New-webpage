500b80a5fb1544c6408fe168513ba0e3b8bd88d1
Abele Atresso <66570173+AbeleAtresso@users.noreply.github.com>
2020-06-29 20:59:22 -0500

    file update

--- script.js
+++ script.js
@@ -4,26 +4,23 @@
 
 var userName = prompt('please enter your first name.'); 
 
-
 alert('Hello, ' + userName + ' click OK to proceed.');
 
 var hourNow = prompt('What time is it, friend? Use Military time.'); 
 
 var greeting; 
 
-if(hourNow >= 0 && hourNow < 7){ 
+if(hourNow >= 0 && hourNow < 6){ 
  greeting = ' Good Morning, ' + userName;
-} else if (hourNow >= 7 && hourNow <= 12){
- greeting = ' Good afternoon ' + userName; 
-} else if(hourNow >= 12 && hourNow <= 18){
-  greeting = ' Good mid day ' + userName; 
-} else if(hourNow >= 18 && hourNow <= 24){
+} else if (hourNow >= 6 && hourNow <= 12){
+  greeting = ' Good afternoon ' + userName; 
+} else if(hourNow >= 12 && hourNow <= 24){
   greeting = ' Good Evening ' + userName; 
 } else {
   'Something went wrong!';
 }
 
-document.write('<h1 style="color:blue">' + greeting + '</h1');
+document.write(`<h1> style="color:blue"> + greeting + </h1`); 
 
 /* 
-This script is displays a greeting to the user based on the users input. */ 
+This script is displays a greeting to the user based on the users input. */
