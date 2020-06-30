44f22ec966e92418c7b40ca7b2ad0279e1cb56cf
Abele Atresso <abele.atresso123@gamil.com>
2020-06-29 19:20:31 -0500

    update file

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ var hourNow = prompt('What time is it, friend? Use Military time.');
 
 var greeting; 
 
-if(hourNow >= 0 && hourNow < 7){ 
+if(hourNow >= 0 && hourNow < 6){ 
  greeting = ' Good Morning, ' + userName;
-} else if (hourNow >= 7 && hourNow <= 12){
- greeting = ' Good Morning ' + userName; 
-} else if (hourNow >= 12 && hourNow <= 18){
+} else if (hourNow >= 6 && hourNow <= 12){
   greeting = ' Good afternoon ' + userName; 
-} else if(hourNow >= 18 && hourNow <= 24){
+} else if(hourNow >= 12 && hourNow <= 24){
   greeting = ' Good Evening ' + userName; 
 } else {
   'Something went wrong!';
